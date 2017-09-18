fc9cecbd6d6c39ca23e2880d49cd0e38a0a4f1a6
Giorgos Andreou <allemaos@gmail.com>
2017-09-18 14:05:50 +0300

    resolved issue for enter to and exit from virtualenv

--- run2.sh
+++ run2.sh
@@ -1,10 +1,14 @@
 #!/bin/zsh
 
+#activate the virtual environment
+source virt_env/virt1/bin/activate
+
 ./dbf2sqlite temptable.dbf -o db/test.db
 
 echo "end of dbftsqlite"
 
 ./test3-sqlite3.py
 
-
+#exit from virtualenv
+deactivate
 
