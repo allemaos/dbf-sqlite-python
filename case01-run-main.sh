5eb040a0f600355b2e76c940f07ea3daaf47089c
Giorgos Andreou <allemaos@gmail.com>
2017-09-18 16:19:13 +0300

    resolved issue with db directory

--- case01-run-main.sh
+++ case01-run-main.sh
@@ -3,6 +3,8 @@
 #activate the virtual environment
 source virt_env/virt1/bin/activate
 
+mkdir -p db
+
 ./dbf2sqlite files/final.dbf -o db/sqlite3.db
 
 ./case01-main.py
--- test-run-main.sh
+++ test-run-main.sh
@@ -4,6 +4,7 @@
 source virt_env/virt1/bin/activate
 
 mkdir -p db
+
 ./dbf2sqlite files/final.dbf -o db/test.db
 
 ./test-run-main-sqlite3.py
